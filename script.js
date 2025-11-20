d82558594968fa45c8eca9c1f62d48665f4f3c0a
unknown <gabriella.jester@yandex.ru>
2025-11-20 18:38:25 +0300

    feat: обновление портфолио

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ export const modules = [
     title: "Фанфики",
     subjects: [
       {
-        name: "Клянись, что не изменишь своему счастью...",
+        name: "«Клянись, что не изменишь своему счастью...»",
         link: "https://disk.yandex.ru/i/eAbOu48wxQLH4Q",
       },
       {
-        name: "Отражение в лезвии ножа.",
+        name: "«Отражение в лезвии ножа»",
         link: "https://disk.yandex.ru/i/7sk07IotxVdo2w",
       },
     ],
@@ -15,18 +15,42 @@ export const modules = [
   {
     title: "Оригинальные произведения",
     subjects: [
-      { name: "Не может быть, что я единственный, кто слышит тебя", link: "https://disk.yandex.ru/i/7HbcT_Eu98OHGg" },
+      {
+        name: "«Не может быть, что я единственный, кто слышит тебя»",
+        link: "https://disk.yandex.ru/i/7HbcT_Eu98OHGg",
+      },
+      {
+        name: "«Ничейные»",
+        link: "https://docs.google.com/document/d/1V0u5CdBwIh5JPa_xLk9SN0ijYcHF9BMSgIJ7Ynw8uUE/edit?usp=sharing",
+      },
+      {
+        name: "«Ничейные» на ЛитРес",
+        link: "https://www.litres.ru/book/emiliya-melnik/nicheynye-72655609/?source_internal=trial_fragment",
+      },
     ],
   },
   {
     title: "Другое",
     subjects: [
       {
-        name: "Черное древо. История.",
+        name: "«Черное древо. История.»",
         link: "https://disk.yandex.ru/i/E1n6EUawXFH0xw",
       },
+      {
+        name: "Глава «Забвение. Часть 2» в формате визуальной новеллы",
+        link: "https://disk.yandex.ru/i/eBuG_7lgP5WWRQ",
+      },
+      {
+        name: "Рассказ «Ничейные» в формате визуальной новеллы",
+        link: "https://disk.yandex.ru/i/1LoPkGRCbIdF9g",
+      },
+      {
+        name: "Глава «Тени» в формате визуальной новеллы",
+        link: "https://disk.yandex.ru/i/drzU35I-_dnZgQ",
+      },
     ],
-  }]
+  },
+];
 
 const container = document.querySelector(".card-container");
 
@@ -45,7 +69,7 @@ modules.forEach((module) => {
     const link = document.createElement("a");
     link.href = subject.link;
     link.textContent = subject.name;
-    link.setAttribute('target', '__blank');
+    link.setAttribute("target", "__blank");
     listItem.appendChild(link);
     list.appendChild(listItem);
   });
